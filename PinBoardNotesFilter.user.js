f0bd290c56fbef3e54b53f161b64f03d286bce68
wenLiangcan <boxeed@gmail.com>
2014-02-28 17:10:56 +0800

    Do not add this link on the notes page.

--- PinBoardNotesFilter.user.js
+++ PinBoardNotesFilter.user.js
@@ -2,7 +2,7 @@
 // @name         PinBoard Notes Filter
 // @description  Add a link to your PinBoard notes
 // @author       wenLiangcan
-// @version      0.1
+// @version      0.2
 // @namespace    https://github.com/wenLiangcan
 // @homepage     https://github.com/wenLiangcan/Userscripts
 // @license      GPL version 3
@@ -16,8 +16,9 @@
 
 (function() {
     var user = document.getElementsByClassName('banner_username')[0].getAttribute('href')
-    if(document.URL.indexOf(user) != -1) {
+    var notes = '/from:notes'
+    if(document.URL.indexOf(user) != -1 && document.URL.indexOf(notes) == -1) {
         var a = document.getElementsByClassName('rss_linkbox')[0]
-        a.insertAdjacentHTML('beforeBegin', '‧  <a href="' + user + '/from:notes/" class="filter ">notes</a>')
+        a.insertAdjacentHTML('beforeBegin', '‧  <a href="' + user + notes + '" class="filter ">notes</a>')
     }
 })()
