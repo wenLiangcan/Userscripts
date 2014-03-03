586cdbf9e2a763c617331c130f7b2f51ba219bc9
wenLiangcan <boxeed@gmail.com>
2014-03-03 15:49:29 +0800

    Code restruction.

--- PinBoardNotesFilter.user.js
+++ PinBoardNotesFilter.user.js
@@ -2,7 +2,7 @@
 // @name         PinBoard Notes Filter
 // @description  Add a link to your PinBoard notes
 // @author       wenLiangcan
-// @version      0.2
+// @version      0.3
 // @namespace    https://github.com/wenLiangcan
 // @homepage     https://github.com/wenLiangcan/Userscripts
 // @license      GPL version 3
@@ -15,10 +15,10 @@
 // ==/UserScript==
 
 (function() {
-    var user = document.getElementsByClassName('banner_username')[0].getAttribute('href')
-    var notes = '/from:notes'
-    if(document.URL.indexOf(user) != -1 && document.URL.indexOf(notes) == -1) {
-        var a = document.getElementsByClassName('rss_linkbox')[0]
-        a.insertAdjacentHTML('beforeBegin', '‧  <a href="' + user + notes + '" class="filter ">notes</a>')
+    var user = document.getElementsByClassName('banner_username')[0].getAttribute('href');
+    var filters = document.getElementsByClassName('filter');
+    if (filters.length !== 0) {
+        var lastFilter = filters[filters.length - 1];
+        lastFilter.insertAdjacentHTML('afterend', ' ‧  <a href="' + user + '/from:notes " class="filter ">notes</a>');
     }
 })()
