a9b4f55aab6d4067bb0714067e827423fa8c7169
wenLiangcan <boxeed@gmail.com>
2016-01-14 00:23:22 +0800

    YunPanWAPRedirect supports album page now.

--- YunPanWAPRedirect.user.js
+++ YunPanWAPRedirect.user.js
@@ -2,11 +2,11 @@
 // @name         Yun Pan WAP Redirect
 // @description  重定向百度云盘的 WAP 页面到普通页面
 // @author       wenLiangcan
-// @version      0.2
+// @version      0.3
 // @namespace    https://github.com/wenLiangcan
 // @homepage     https://github.com/wenLiangcan/Userscripts
 // @license      GPL version 3 (http://www.gnu.org/licenses/gpl.txt)
-// @copyright    Copyright © 2014-2015 wenLiangcan
+// @copyright    Copyright © 2014-2016 wenLiangcan
 // @updateURL
 // @downloadURL
 // @include      http://*
@@ -21,5 +21,8 @@
     }
     else if (/^http:\/\/pan\.baidu\.com\/wap\/share\/.*?$/.test(document.URL)) {
         window.location.href = document.URL.replace('wap/', '');
+    }
+    else if (/^http:\/\/pan\.baidu\.com\/wap\/album\/.*?$/.test(document.URL)) {
+        window.location.href = document.URL.replace('wap', 'pcloud');
     }
 })();
